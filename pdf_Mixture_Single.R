d664abb8da9eedb29734a6978f89f3d7a7b8358e
Mark Greenaway <markg@maths.usyd.edu.au>
2017-04-20 14:49:49 +1000

    First pass of translation

--- pdf_Mixture_Single.R
+++ pdf_Mixture_Single.R
@@ -3,13 +3,13 @@ pdf_Mixture_Single <- function(x,p,mu,sigma){
     global xDomain
     global pseudo_uniform){){
 
-    pseudo_normal in normpdf(xDomain,mu,sigma).*pseudo_uniform){){
+    pseudo_normal <- normpdf(xDomain,mu,sigma)*pseudo_uniform
 
-    normFactor_uniform in sum(pseudo_uniform)){){
+    normFactor_uniform <- sum(pseudo_uniform)
     normFactor_normal <- sum(pseudo_normal)
 
-    if (normFactor_uniform  in  in  0){){
-        normFactor_uniform in 10^-8){){
+    if (normFactor_uniform  == 0){){
+        normFactor_uniform <- 10^-8
     }
 
     if (normFactor_normal == 0){
@@ -17,19 +17,19 @@ pdf_Mixture_Single <- function(x,p,mu,sigma){
     }
 
 
-    uniResultTemp in interp1(xDomain, pseudo_uniform, x)){){
-    normResultTemp <- normpdf(x,mu,sigma).*uniResultTemp
+    uniResultTemp <- interp1(xDomain, pseudo_uniform, x)
+    normResultTemp <- normpdf(x,mu,sigma)*uniResultTemp
 
-    uniResultTemp in uniResultTemp/normFactor_uniform){){
+    uniResultTemp <- uniResultTemp/normFactor_uniform)
     normResultTemp <- normResultTemp/normFactor_normal
 
     propNorm <- p
-    propUniform in 1-p){){
+    propUniform <- 1-p
 
     normResult <- propNorm*normResultTemp
-    uniResult in propUniform*uniResultTemp){){
+    uniResult <- propUniform*uniResultTemp
 
-    if (sum(size(normResult)==size(uniResult))==2){
+    if (sum(length(normResult)==length(uniResult))==2){
         tempResult <- normResult+uniResult
     } else {
         tempResult <- normResult+uniResult'
