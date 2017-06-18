ee5d3a007bc239aef9e4d2d6f1541bb8acc719f5
Mark Greenaway <markg@maths.usyd.edu.au>
2017-06-18 15:18:35 +1000

    Port pdf_Mixture_Single function to R

--- pdf_Mixture_Single.R
+++ pdf_Mixture_Single.R
@@ -1,28 +1,52 @@
+library(signal) # Provides interp1 function
+# interp1 <- function(x, v, xq)
+# {
+#   cat("x ", x, " v ", v, " xq ", xq, "\n")
+#   x2 <- x[order(x)]
+#   v2 <- v[order(x)]
+#   
+#   vq <- rep(0, length(xq))
+#   for (idx in 1:length(xq)) {
+#     i <- 1
+#     while (xq[idx] >= x2[i]) i = i + 1
+# 
+#     # Now x[i - 1] < xq and x[i] >= xq
+#     m <- (v2[i] - v2[i - 1]) / (x2[i] - x2[i - 1])
+#     vq[idx] <- v2[i - 1] + m * (xq[idx] - x2[i - 1])
+#   }
+# 
+#   return(vq)
+# }
+
 pdf_Mixture_Single <- function(x,p,mu,sigma){
 
-    global xDomain
-    global pseudo_uniform){){
-
-    pseudo_normal <- normpdf(xDomain,mu,sigma)*pseudo_uniform
-
+    pseudo_normal <- dnorm(xDomain,mu,sigma)*pseudo_uniform
+    cat("pseudo_normal ", pseudo_normal, "\n")  
+    
     normFactor_uniform <- sum(pseudo_uniform)
+    cat("normFactor_uniform", normFactor_uniform, "\n")  
     normFactor_normal <- sum(pseudo_normal)
-
-    if (normFactor_uniform  == 0){){
+    cat("normFactor_normal", normFactor_normal, "\n")  
+    
+    if (normFactor_uniform  == 0 || is.nan(normFactor_uniform)){
         normFactor_uniform <- 10^-8
     }
 
-    if (normFactor_normal == 0){
+    if (normFactor_normal == 0 || is.nan(normFactor_normal)){
         normFactor_normal <- 10^-8
     }
 
 
     uniResultTemp <- interp1(xDomain, pseudo_uniform, x)
-    normResultTemp <- normpdf(x,mu,sigma)*uniResultTemp
-
-    uniResultTemp <- uniResultTemp/normFactor_uniform)
+    #cat("uniResultTemp ", uniResultTemp, "\n")
+    normResultTemp <- dnorm(x,mu,sigma)*uniResultTemp
+    #cat("normResultTemp ", normResultTemp, "\n")
+    
+    uniResultTemp <- uniResultTemp/normFactor_uniform
+    #cat("uniResultTemp ", uniResultTemp, "\n")
     normResultTemp <- normResultTemp/normFactor_normal
-
+    #cat("normResultTemp ", normResultTemp, "\n")
+    
     propNorm <- p
     propUniform <- 1-p
 
@@ -32,11 +56,12 @@ pdf_Mixture_Single <- function(x,p,mu,sigma){
     if (sum(length(normResult)==length(uniResult))==2){
         tempResult <- normResult+uniResult
     } else {
-        tempResult <- normResult+uniResult'
+        tempResult <- normResult+t(uniResult)
     }
 
     #xIndex = x-min(xDomain)+1;
     #results = tempResult(xIndex);
-    result <- tempResult
-
+    #cat("tempResult ", tempResult, "\n")
+    #tempResult <- tempResult[1]
+    return(tempResult)
 }
